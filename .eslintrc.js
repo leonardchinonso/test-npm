8460706067b4efbad7d2858b20bb4b21e81f0cb5
Leonard Chinonso <leokingluthers@gmail.com>
2021-03-16 11:26:39 +0100

    Setup Package

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+const OFF = 0;
+const WARN = 1;
+
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    env: {
+        browser: true,
+        es6: true,
+        mocha: true,
+    },
+    extends: [
+        "airbnb-base",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    rules: {
+        indent: [WARN, 4,],
+        "linebreak-style": OFF,
+        quotes: [WARN, "double",],
+        semi: [WARN, "always",],
+        "max-len": [WARN, 150,],
+        "comma-dangle": [WARN, "always",],
+        "no-cond-assign": [WARN, "always",],
+        "no-console": WARN,
+        "no-underscore-dangle": OFF,
+        "comma-spacing": OFF,
+        camelcase: WARN,
+        "no-unused-vars": OFF,
+        "padded-blocks": OFF,
+        "arrow-body-style": OFF,
+        "@typescript-eslint/no-explicit-any": OFF,
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                js: "never",
+                jsx: "never",
+                ts: "never",
+                tsx: "never",
+            },
+        ],
+    },
+    settings: {
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".jsx", ".ts", ".tsx",],
+            },
+        },
+    },
+};
---
+++ src/index.ts
@@ -0,0 +1,151 @@
+import * as crypt from "bcryptjs";
+import mongoose, { Schema, Document, } from "mongoose";
+import * as _ from "lodash";
+import beautifyUnique from "mongoose-beautiful-unique-validation";
+
+// INTERFACES
+// UserInterface
+export interface UserInterface extends Document{
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    passwordResetToken: string;
+    phoneNumber: string;
+    emailVerificationToken: string;
+    emailVerified: boolean;
+}
+
+// NoteInterface
+export interface NoteInterface extends Document{
+    createdBy: mongoose.Types.ObjectId;
+    title: string;
+    body: string;
+}
+
+// TokenInterface
+export interface TokenInterface extends Document{
+    userId: mongoose.Types.ObjectId;
+    token: any[];
+}
+
+const UserSchema: Schema = new Schema({
+    firstName: {
+        type: String,
+        trim: true,
+    },
+    middleName: {
+        type: String,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        minlength: [6, "Password should be more than 5 characters.",],
+    },
+    passwordResetToken: {
+        type: String,
+    },
+    phoneNumber: {
+        type: String,
+    },
+    emailVerificationToken: {
+        type: String,
+    },
+    emailVerified: {
+        type: Boolean,
+        default: false,
+    },
+},);
+
+UserSchema.plugin(beautifyUnique, { defaultMessage: "{VALUE} has been taken.", },);
+
+UserSchema.methods = {
+    toJSON(): Record<string, unknown> {
+        const userObject = this.toObject();
+        return _.pick(userObject,
+            ["_id", "email", "firstName", "middleName", "lastName", "phoneNumber", "emailVerified",
+            ],);
+    },
+};
+
+// eslint-disable-next-line func-names
+UserSchema.pre<UserInterface>("save", async function (next,) {
+    if (this.isModified("password",)) {
+        const salt = await crypt.genSalt(10,);
+        this.password = await crypt.hash(this.password, salt,);
+    }
+    next();
+},);
+
+export const User = mongoose.connection.useDb("all-in-one-auth",).model<UserInterface>("User", UserSchema,);
+
+// TokenSchema
+const TokenSchema: Schema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        unique: true,
+        required: true,
+    },
+    token: [{
+        value: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        lastSeen: {
+            type: Date,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+    },],
+},);
+
+TokenSchema.methods = {
+    toJSON(): Record<string, unknown> {
+        const tokenObject = this.toObject();
+        return _.pick(tokenObject,
+            ["_id", "userId", "token",],);
+    },
+};
+
+export const Token = mongoose.connection.useDb("all-in-one-auth",).model<TokenInterface>("Token", TokenSchema,);
+
+// NoteSchema
+const NoteSchema: Schema = new Schema({
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+},);
+
+NoteSchema.methods = {
+    toJSON(): Record<string, unknown> {
+        const noteObject = this.toObject();
+        return _.pick(noteObject,
+            ["_id", "createdBy", "title", "body",],);
+    },
+};
+
+export const Note = mongoose.connection.useDb("all-in-one-notes",).model<NoteInterface>("Note", NoteSchema,);
