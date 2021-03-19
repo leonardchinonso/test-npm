841480ed71953f3a37d8bcccfa9d3ddb2828fb16
Leonard Chinonso <leokingluthers@gmail.com>
2021-03-19 14:11:25 +0100

    Debugging - added connect func to index

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,16 @@
-/* eslint-disable no-console */
-import mongoose from "mongoose";
-
-mongoose.Promise = global.Promise;
-
-const uri = "mongodb://127.0.0.1:27017/octopus?authSource=admin";
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, },);
-
-const db = mongoose.connection.once("open", () => {
-    console.log("MongoDb connection created successfully!",);
-},).on("error", () => {
-    console.log("MongoDB connection error:",);
-},);
-
-export default db;
+// /* eslint-disable no-console */
+// import mongoose from "mongoose";
+//
+// mongoose.Promise = global.Promise;
+//
+// const uri = "mongodb://127.0.0.1:27017/octopus?authSource=admin";
+//
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, },);
+//
+// const db = mongoose.connection.once("open", () => {
+//     console.log("Test-npm-390 MongoDb connection created successfully!",);
+// },).on("error", () => {
+//     console.log("Test-npm-390 MongoDB connection error:",);
+// },);
+//
+// export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import "./db";
 import * as crypt from "bcryptjs";
 import mongoose, { Schema, Document, } from "mongoose";
 import * as _ from "lodash";
 import beautifyUnique from "mongoose-beautiful-unique-validation";
+
+export function connect(uri: string,) {
+
+    mongoose.Promise = global.Promise;
+    // const uri = "mongodb://127.0.0.1:27017/octopus?authSource=admin";
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, },);
+    const db = mongoose.connection.once("open", () => {
+        console.log("Test-npm-390 MongoDb connection created successfully!",);
+    },).on("error", () => {
+        console.log("Test-npm-390 MongoDB connection error:",);
+    },);
+}
 
 // INTERFACES
 // UserInterface
@@ -150,3 +161,5 @@ NoteSchema.methods = {
 };
 
 export const Note = mongoose.connection.useDb("all-in-one-notes",).model<NoteInterface>("Note", NoteSchema,);
+
+
